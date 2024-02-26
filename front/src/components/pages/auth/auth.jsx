cb7da191e695a402904a5b9a087dc12010e57d3b
oltree <oleg.melekh.frontend@gmail.com>
2024-02-26 19:38:07 +0300

    fix: fix auth

--- front/src/components/pages/auth/auth.jsx
+++ front/src/components/pages/auth/auth.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { login, register } from '../../../store/slices/auth';
 
 import styles from './auth.module.scss';
-
 export const Auth = () => {
   const dispatch = useDispatch();
   const isLogin = useSelector(state => state.auth.isLogin);
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = () => {
     if (isLogin) {
       dispatch(login({ email, password }));
+      navigate('/');
     } else {
       dispatch(register({ email, password }));
+      navigate('/login');
     }
   };
 
+  useEffect(() => {
+    if (!isLogin) {
+      navigate('/register');
+    }
+  }, [isLogin, navigate]);
+
   return (
     <div className={styles.wrapper}>
-      <form onSubmit={e => e.preventDefault()}>
-        <h1>{isLogin ? 'Login' : 'register'}</h1>
+      <form className={styles.form} onSubmit={e => e.preventDefault()}>
+        <h1 className={styles.title}>{isLogin ? 'login' : 'register'}</h1>
 
         <input
           type='email'
           value={email}
           onChange={e => setEmail(e.target.value)}
           placeholder='Email...'
+          className={styles.input}
         />
 
         <input
@@ -37,9 +46,10 @@ export const Auth = () => {
           value={password}
           onChange={e => setPassword(e.target.value)}
           placeholder='Password...'
+          className={styles.input}
         />
 
-        <div>
+        <div className={styles.buttons}>
           <button type='submit' onClick={handleSubmit}>
             {isLogin ? 'Login' : 'Register'}
           </button>
